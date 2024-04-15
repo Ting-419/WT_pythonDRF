a4f90115fe46c79939fde6e4dc215266a9801c23
Ting-419 <longting921@gmail.com>
2024-04-15 23:46:20 +0300

    Ting Long Homework04

---
+++ custom_user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import UserList, UserUpdate
+
+
+urlpatterns = [
+    path('users/', UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserUpdate.as_view(),name='user-update'),
+]
+
+
